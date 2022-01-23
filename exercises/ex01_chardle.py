13c7ba2574e13f1e52f49509a1377d06db77f450
Grace Blake <gracebla@live.unc.edu>
2022-01-23 17:17:03 -0500

    Progress to ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -12,6 +12,9 @@ if len(word_variable) != 5:
 letter_variable: str = input("Enter a single character: ")
 print("Searching for " + letter_variable + " in " + word_variable)
 
+if len(letter_variable) != 1:
+    print("Error: Character must be a single character.")
+    exit()
 
 if letter_variable == word_variable[0]: 
     print(letter_variable + " found at index 0")
@@ -26,11 +29,12 @@ if letter_variable == word_variable[4]:
    
 
 ans = word_variable.count(letter_variable)
-if ans == 1:
-    print("1 instance of " + letter_variable + " found in " + word_variable)
-if ans == 2: 
-    print("2 instances of " + letter_variable + " found in " + word_variable)
-if ans == 3: 
-    print("3 instances of " + letter_variable + " found in " + word_variable)   
-if ans == 0: 
+if ans >= 1:
+    if ans == 1: 
+        print("1 instance of " + letter_variable + " found in " + word_variable)
+    if ans == 2: 
+        print("2 instances of " + letter_variable + " found in " + word_variable)
+    if ans == 3: 
+        print("3 instances of " + letter_variable + " found in " + word_variable)   
+else:
     print("No instances of " + letter_variable + " found in " + word_variable)
