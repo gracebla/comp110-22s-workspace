d1b85186f0e4dc5648ae00754b75392c29b3adf2
Grace Blake <gracebla@live.unc.edu>
2022-01-24 16:24:03 -0500

    update to ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -6,15 +6,16 @@ __author__ = "730224294"
 word_variable: str = input("Enter a 5-character word: ")
 
 if len(word_variable) != 5:
-    print("Error: Word  must contain 5 characters")
+    print("Error: Word must contain 5 characters")
     exit()
 
 letter_variable: str = input("Enter a single character: ")
-print("Searching for " + letter_variable + " in " + word_variable)
 
 if len(letter_variable) != 1:
     print("Error: Character must be a single character.")
     exit()
+
+print("Searching for " + letter_variable + " in " + word_variable)
 
 if letter_variable == word_variable[0]: 
     print(letter_variable + " found at index 0")
@@ -32,7 +33,6 @@ ans = word_variable.count(letter_variable)
 if ans == 1:
     print(str(ans) + " instance of " + letter_variable + " found in " + word_variable)
 else: 
-
     if ans > 1:
         print(str(ans) + " instances of " + letter_variable + " found in " + word_variable)
     else:
