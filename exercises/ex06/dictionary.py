1326a2b7023f8c6648b4723c609b5245c287b7cd
Grace Blake <gracebla@live.unc.edu>
2022-02-28 10:45:50 -0500

    ex06 update

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -7,5 +7,31 @@ def invert(first: dict[str, str]) -> dict[str, str]:
     """Inverts the keys and values."""
     return_dict: dict[str, str] = {}
     for key in first:
+        if first[key] in return_dict:
+            raise KeyError("Key already exists.")
         return_dict[first[key]] = key
     return return_dict
+
+
+def favorite_color(color: dict[str, str]) -> str:
+    """Returns most common favorite color from a dictionary of favorite colors.""" 
+    return_dict: dict[str, int] = {}
+    final_color: str = ''
+    final_variable: int = 0
+    for key in color:
+        current_color = color[key] 
+        if color[key] in return_dict:
+            return_dict[current_color] += 1
+        else: 
+            return_dict[current_color] = 1
+    for key in return_dict:
+        if return_dict[key] > final_variable:
+            final_variable = return_dict[key]
+            final_color = key
+    return final_color 
+
+
+def count(counter: list[str]) -> dict[str, int]:
+    """Creates a dictionary that shows how many times certain strings were present in a list."""
+    new_dict: dict[str, int] = {}
+    
---
+++ exercises/ex06/dictionary_test.py
@@ -0,0 +1,47 @@
+"""Space to run tests for dictionary."""
+
+__author__ = 730224294
+
+from dictionary import invert, favorite_color
+import pytest
+
+
+def test_invert() -> None: 
+    """Use case that tests short dictionary for inverting keys and values."""
+    xs: dict[str, str] = {'UNC': 'win', 'DUKE': 'lose'}
+    assert invert(xs) == {'win': 'UNC', 'lose': 'DUKE'}
+
+
+def test_invert_empty() -> None:
+    """Edge case that tests empty dictionary for inverting."""
+    xs: dict[str, str] = {}
+    assert invert(xs) == {}
+
+
+def test_invert_second() -> None: 
+    """Second use case that tests short dictionary for inverting keys and values."""
+    xs: dict[str, str] = {'grace': 'chocolate', 'sara': 'vanilla'}
+    assert invert(xs) == {'chocolate': 'grace', 'vanilla': 'sara'}
+
+
+def test_favorite_color_empty() -> None:
+    """Edge case that returns empty string for most common color."""
+    xs: dict[str, str] = {}
+    assert favorite_color(xs) == ''
+
+
+def test_favorite_color() -> None:
+    """Use case to return most common color."""
+    xs: dict[str, str] = {'grace': 'blue', 'sarah': 'pink', 'riley': 'blue'}
+    assert favorite_color(xs) == 'blue'
+
+
+def test_favorite_color_second() -> None:
+    """Second use case to return most common color with multiple favorite colors."""
+    xs: dict[str, str] = {'grace': 'pink', 'abbie': 'pink', 'lauren': 'gray', 'scott': 'yellow', 'jim': 'yellow', 'harry': 'blue'}
+    assert favorite_color(xs) == 'pink'
+
+
+with pytest.raises(KeyError):
+    my_dictionary = {'Grace': 'Blake', 'Kathryn': 'Blake'}
+    invert(my_dictionary)
